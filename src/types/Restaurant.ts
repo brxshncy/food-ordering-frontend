c4b32e01b92f2d6b5fd093a171953ff6e274ec10
brxshncy <ggkidsbruce619@gmail.com>
2024-07-21 19:48:48 +0800

    progress in frontend

---
+++ src/types/Restaurant.ts
@@ -0,0 +1,18 @@
+export type MenuItemType = {
+  _id: string;
+  name: string;
+  price: number;
+};
+export type Restaurant = {
+  _id: string;
+  user: string;
+  restaurantName: string;
+  city: string;
+  country: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItemType[];
+  imageUrl: string;
+  lastUpdated: string;
+};
